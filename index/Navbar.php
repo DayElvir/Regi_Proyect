d0e5853b12502bf03212a269954affa4cee22b9d
Days-hug <dayanaelvir4@gmail.com>
2024-02-25 20:36:45 -0600

    cambios

--- index/Navbar.php
+++ index/Navbar.php
@@ -1,7 +1,7 @@
-<?php
- include("./conexionbd.php");
 
+<?php include("");
 ?>
+
 
 
 <!DOCTYPE html>
@@ -11,7 +11,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <link rel="stylesheet" href="css/Navbar.css">
+  <link rel="stylesheet" href="Navbar.css">
   <title>Navbar</title>
 </head>
 <body>
