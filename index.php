1e2683cf8d3a43b1104815a818eb035a90e727db
Days-hug <dayanaelvir4@gmail.com>
2024-02-25 18:08:50 -0600

    conexion

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
- echo" Hola Mundo ";
+ include("./conexionbd.php");
 
- echo " Se puede lograr ";
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGI</title>
+</head>
+<body>
+    
+</body>
+</html>
