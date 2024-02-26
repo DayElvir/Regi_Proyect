bf61b6f72a4954060a1e2392dcccec566faf58bf
Days-hug <dayanaelvir4@gmail.com>
2024-02-25 18:06:51 -0600

    prueba_conexion

--- conexionbd.php
+++ conexionbd.php
@@ -1,11 +1,23 @@
-<?php  
-    
-        $conexion =mysqli_connect("localhost", "root","20191003101", "bd_regi");   
-   
-        if($conexion){     
-        echo'conectado a la base de datos exito';             
-      }else{      
-        echo 'no se pudo hacer la conexion';    
-    }
-    
+<?php
+  $servidor="localhost";
+  $database="bd_regi";
+  $usuario= "root";
+  $contrasena= "20191003101";
+  
+  
+  try { 
+  
+    $conexion =new PDO("mysql:host= $servidor; 
+    dbname= $database",$usuario , $contrasena ); 
+
+      echo"conexion realizada....";
+
+  } catch (Exception $error) {
+  
+    echo  $error->getMessage() ."";
+  }
+
+
+
+
 ?>
