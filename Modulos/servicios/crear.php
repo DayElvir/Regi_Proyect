580037ad7a756e8b0cc689019c9afc6d2e7e5f24
Days-hug <dayanaelvir4@gmail.com>
2024-02-26 01:08:03 -0600

    actualizacion

--- Modulos/servicios/crear.php
+++ Modulos/servicios/crear.php
@@ -5,32 +5,40 @@ include("../../conexionbd.php");
 
 if($_POST){
    //Almacenacion de datos o recepcion
-   $nombre=isset($_POST["nombre"]) ? $_POST["nombre"] :"";
-   $descripcion=isset($_POST["descripcion"]) ? $_POST["descripcion"] :"";
-   $costo=isset($_POST["costo"]) ? $_POST["costo"] :"";
-   $inicio=isset($_POST["inicio"]) ? $_POST["inicio"] :"";
-   $final=isset($_POST["final"]) ? $_POST["final"] :"";
-   $creacion=isset($_POST["creacion"]) ? $_POST["creacion"] :"";
+   $nombre=(isset($_POST['nombreservicio'])) ? $_POST['nombreservicio'] :"";
+   $descripcion=(isset($_POST['descripcion'])) ? $_POST['descripcion'] :"";
+   $costoservicio=(isset($_POST['costoservicio'])) ? $_POST['costoservicio'] :"";
+   $iniciar=(isset($_POST['iniciar'])) ? $_POST['iniciar'] :"";
+   $final=(isset($_POST['final'])) ? $_POST['final'] :"";
+   $creacion=(isset($_POST['creacion'])) ? $_POST['creacion'] :"";
      
-   //echo $descripcion;
-  //conexion de base de datos
- $sentencia=$conexion->prepare(" INSERT INTO `servicio` 
- (`ID`,`nombre_servicio`, `desc_servicio`, 
- `cost_servicio`, `fech_inicio`, `fech_final`, 
- `actualizado_por`) VALUES
-  (NULL, :nombre,:descripcion,
-   :costo, :inicio, :final, :creacion); ");
-    $sentencia->bindParam(":nombre",$nombre);
-    $sentencia->bindParam(":descripcion",$descripcion);
-    $sentencia->bindParam(":costo",$costo);
-    $sentencia->bindParam(":inicio",$inicio);
-   $sentencia->bindParam(":final",$final);
-   $sentencia->bindParam(":creacion",$creacion);
-   //$sentencia->execute();
+   $sentencia=$conexion->prepare("INSERT INTO `servicios` (`idservicio`, `nombre_servicio`, `desc_servicio`, `cost_servicio`, `fech_inicio`, `fech_final`, `actualizado_por`) 
+   VALUES (NULL, :nombreservicio, :descripcion, :costoservicio , :iniciar, :final, :creacion);");
+   
+
+  $sentencia->bindParam(  ":nombreservicio",$nombre  );
+  $sentencia->bindParam(":descripcion",$descripcion );
+  $sentencia->bindParam(":costoservicio",$costoservicio );
+  $sentencia->bindParam(":iniciar",$iniciar);
+$sentencia->bindParam(":final",$final);
+$sentencia->bindParam(":creacion",$creacion);
+
+
+   $sentencia->execute();
+    
+ 
+
+
+
+
+  
 }
 
-//include("../../header.php");
+include("../../header.php");
 ?>
+
+
+
 <div class="card">
     <div class="card-header">
         Crear Servicio</div>
@@ -42,14 +50,14 @@ if($_POST){
             <input
                 type="text"
                 class="form-control"
-                name="nombre"
-                id="nombre"
+                name="nombreservicio"
+                id="nombreservicio"
                 aria-describedby="helpId"
                 placeholder=" Escriba el nombre del servicio "  />
         </div>
         
         <div class="mb-3">
-            <label for="Descrip" class="form-label">Descripción</label>
+            <label for="descripcion" class="form-label">Descripción</label>
             <input
                 type="text"
                 class="form-control"
@@ -62,24 +70,27 @@ if($_POST){
         <div class="mb-3">
             <label for="costoservicio" class="form-label">Costo</label>
             <input
-                type="number"
+                type="numeric"
                 class="form-control"
-                name="costo"
-                id="costo"
+                name="costoservicio"
+                id="costoservicio"
                 aria-describedby="helpId"
                 placeholder=" Escriba el precio a pagar "  />
         </div>
       
         <div class="mb-3">
-            <label for="inicio" class="form-label">Fecha de Inicio</label>
+            <label for="iniciar" class="form-label">Iniciar Pago</label>
             <input
                 type="date"
                 class="form-control"
-                name=inicio"
-                id="inicio"
+                name="iniciar"
+                id="iniciar"
                 aria-describedby="helpId"  />
         </div>
-        
+
+
+
+
 
         <div class="mb-3">
             <label for="final" class="form-label">Fecha final</label>
@@ -92,7 +103,7 @@ if($_POST){
         </div>
    
         <div class="mb-3">
-            <label for="creado" class="form-label">Creado por:</label>
+            <label for="creacion" class="form-label">Creado por:</label>
             <input
                 type="text"
                 class="form-control"
@@ -121,8 +132,7 @@ if($_POST){
 
 
     </div>
-    <div class="card-footer text-muted">Footer</div>
-</div>
+  
 
 
 
